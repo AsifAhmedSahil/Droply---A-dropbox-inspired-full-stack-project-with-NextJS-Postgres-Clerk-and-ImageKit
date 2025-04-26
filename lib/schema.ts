0a4d1f8f7e877af3ad784f13643f8a0e2c9806a3
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-04-26 16:35:59 +0600

    schema design:

---
+++ lib/schema.ts
@@ -0,0 +1,34 @@
+import {pgTable,text,uuid,integer,boolean, timestamp} from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+
+export const files = pgTable("files",{
+    id:uuid("id").defaultRandom().primaryKey(),
+    // basic file information
+
+    name:text("name").notNull(),
+    path:text("path").notNull(),
+    size:integer("size").notNull(),
+    type:text("type").notNull(),
+
+    // storage information
+
+    fileUrl:text("file_url").notNull(), // use to access the file
+    thumbnailUrl:text("thumbnail_url").notNull(),
+
+    // ownership
+
+    userId: text("user_id").notNull(), 
+    parentId: text("parent_id"),// 
+
+    // file/folder flag
+    isFolder: boolean("is_folder").default(false).notNull(),
+    isStarred:boolean("is_starred").default(false).notNull(),
+    isTrashed:boolean("is_trashed").default(false).notNull(),
+
+    // TimeStamp
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+
+
+})
