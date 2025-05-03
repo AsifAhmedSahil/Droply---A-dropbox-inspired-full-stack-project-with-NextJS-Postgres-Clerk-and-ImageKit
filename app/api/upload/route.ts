9e7a95fee7e8443467baf212a669e380049bea44
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-03 23:48:23 +0600

    update

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,23 +3,38 @@ import ImageKit from "imagekit";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  const { userId } = await auth();
-  if (!userId) {
-    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
-  }
+ try {
+   const { userId } = await auth();
+   if (!userId) {
+     return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+   }
+ 
+   const body = request.json();
+   const { imagekit, userId: bodyUserId } = body;
+   if (bodyUserId !== userId) {
+     return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+   }
+ 
+   if (!imagekit || !imagekit.url) {
+     return NextResponse.json({ error: "invalid file upload" }, { status: 401 });
+   }
+ 
+   const fileData = {
+     name: imagekit.name || "Untitled",
+     path: imagekit.filePath || `/droply/${userId}/${imagekit.name}`,
+     size: imagekit.size || 0,
+     type: imagekit.fileType || "image",
+     fileUrl: imagekit.url,
+     thumbnailUrl: imagekit.thumbnailUrl || null,
+     userId: userId,
+     parentId: null,
+     isFolder: false,
+     isStarred: false,
+     isTrash: false,
+   };
 
-  const body = request.json()
-  const {imagekit,userId: bodyUserId} = body
-  if(bodyUserId !== userId){
-    return NextResponse.json({ error: "unauthorized" }, { status: 401 });
-  }
-
-  if(!imagekit || !imagekit.url){
-    return NextResponse.json({ error: "invalid file upload" }, { status: 401 });
-  }
-
+   
+ } catch (error) {
   
-
-
-
+ }
 }
