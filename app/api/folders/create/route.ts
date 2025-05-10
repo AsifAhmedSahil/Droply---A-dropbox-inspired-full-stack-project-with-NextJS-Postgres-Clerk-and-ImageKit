f53d4df51305e7e45a5127c8676204cf17744aae
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2025-05-10 23:55:11 +0600

    folder route:

--- app/api/folders/create/route.ts
+++ app/api/folders/create/route.ts
@@ -1,4 +1,7 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 
@@ -15,8 +18,35 @@ export async function POST(request:NextRequest) {
         }
 
         if(!name || typeof name !== "string" || name.trim() === ""){
-            return NextResponse.json({error:"Folder name is required!"},{status:401})
+            return NextResponse.json({error:"Folder name is required!"},{status:400})
         }
+
+        if(parentId){
+           const [parentFolder] =  await db.select().from(files).where(
+                and(
+                    eq(files.id,parentId),
+                    eq(files.userId,userId),
+                    eq(files.isFolder,true)
+                )
+            )
+            if(!parentFolder){
+                return NextResponse.json({error:"Parent Folder not found!"},{status:401})
+            }
+        }
+
+        // create a folder in database***
+
+        const folderData = {
+                id:uuid
+        }
+
+        const [newFolder] = await db.insert(files).values(folderData).returning()
+
+        return NextResponse.json({
+            success:true,
+            mesage:"Folder created successfully!",
+            folder:newFolder
+        })
 
         
     } catch (error) {
